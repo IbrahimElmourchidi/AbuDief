b9a9b7975488f6e5841f3de023c8352890f39c70
Mahmoud-Daif <mahmoud.abodaf123@gmail.com>
2024-04-26 03:25:50 +0300

    collections lessson

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,55 @@ var lastName='mohamed';
 print(lastName);
 print(z);
 print(firstName);
+////////////////////////////////////////////arthimatic operator/////////////////////////////////////
+int xx=5;
+int yy=10;
+double zz=10;
+//int summ=xx/yy;      error becuase type of sum
+int summ=xx~/yy; 
+num s=xx/yy;
+double ss=xx/yy;
+int remainder=yy%xx;
 
+//////////////////////////////// dot operator ///////////////////////////////
+String fullName="Mahmoud Abu Deif";
+print(fullName.length);
+print(fullName.codeUnits);
+print(fullName.isEmpty);
+print(fullName.toUpperCase());
+int a=10;
+print(a.isEven);
+print(a.isNaN);
+print(a.isNegative);
+print(a.toDouble());
+////////////////////////////////////////////// Collections  =>List, set and map   ////////////////////////////////
+List<String> studentName=['Mahmoud','Hema','omar'];
+print(studentName);
+print(studentName[0]);
+print(studentName[1]);
+
+Set<String> stName={'Mahmoud','Hema','omar','omar'}; // set store unique values
+print(stName);
+print(stName.length);
+print(stName.first);
+stName.add('Ali');
+print(stName);
+stName.remove('Ali');
+print(stName);
+
+Map<String,int> ages={
+  'Mahmoud':25
+  ,'Ali':20,
+  'omar':18
+};
+print(ages);
+print(ages['Mahmoud']);
+Map<int,int > numberOfSeatsInRoom={
+1:25,
+2:20,
+4:30
+};
+numberOfSeatsInRoom[3]=10;
+print(numberOfSeatsInRoom);
 
 }
